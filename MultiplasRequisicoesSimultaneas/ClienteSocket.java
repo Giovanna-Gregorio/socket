24dd790039f242ce192a45707adabdd01b69ca8c
Giovanna Gregório Guimarães <giovanna_gregorio@univem.edu.br>
2022-02-22 21:33:05 -0300

    socket tcp que atende a múltiplas requisições simultâneas

---
+++ MultiplasRequisicoesSimultaneas/ClienteSocket.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class ClienteSocket extends Thread {
+    private Socket conexao;
+    public ClienteSocket(Socket socket) {
+        this.conexao = socket;
+    }
+    public static void main(String args[])
+    {
+        try {
+            Socket socket = new Socket("127.0.0.1", 5555);
+            PrintStream saida = new PrintStream(socket.getOutputStream());
+            BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Digite seu nome: ");
+            String meuNome = teclado.readLine();
+            saida.println(meuNome.toUpperCase());
+            Thread thread = new ClienteSocket(socket);
+            thread.start();
+            String msg;
+            while (true)
+            {
+                System.out.print("Mensagem > ");
+                msg = teclado.readLine();
+                saida.println(msg);
+            }
+        } catch (IOException e) {
+            System.out.println("Falha na Conexao... .. ." + " IOException: " + e);
+        }
+    }
+
+    public void run()
+    {
+        try {
+            BufferedReader entrada = 
+                new BufferedReader(new InputStreamReader(this.conexao.getInputStream()));
+
+            String msg;
+            while (true)
+            {
+                msg = entrada.readLine();
+                if (msg == null) {
+                    System.out.println("Conexão encerrada!");
+                    System.exit(0);
+                }
+                System.out.println();
+                System.out.println(msg);
+                System.out.print("Responder > ");
+            }
+        } catch (IOException e) {
+            System.out.println("Ocorreu uma Falha... .. ." + 
+                " IOException: " + e);
+        }
+    }
+}
