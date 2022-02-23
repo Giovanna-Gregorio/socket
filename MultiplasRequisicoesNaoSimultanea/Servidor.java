86630aa182c8d26718c55533b5cef762182d68c7
Giovanna Gregório Guimarães <giovanna_gregorio@univem.edu.br>
2022-02-22 21:31:41 -0300

    socket tcp que atende multiplas requisições não simultânea

---
+++ MultiplasRequisicoesNaoSimultanea/Servidor.java
@@ -0,0 +1,27 @@
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class Servidor {
+    public static void main(String[] args) {
+        try {
+          ServerSocket servidor = new ServerSocket(3320);
+          System.out.println("Servidor ouvindo a porta 3320");
+          
+          while(true) {
+            Socket cliente = servidor.accept();
+            System.out.println("Cliente conectado: " + cliente.getInetAddress().getHostAddress());
+            
+            ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
+            saida.flush();
+            saida.writeObject(new Date());
+            saida.close();
+            cliente.close();
+          }
+        }
+        catch(Exception e) {
+           System.out.println("Erro: " + e.getMessage());
+        }
+  }
+}
